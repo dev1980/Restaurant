7c0ee25394f55637ca8d66417fc7a0113613b42c
dev1980 <brham1980@gmail.com>
2020-02-06 20:48:54 +0545

    linter error fiexed

--- src/about.js
+++ src/about.js
@@ -1,18 +1,18 @@
 const about = () => {
-    let content = document.querySelector('main');
-    content.innerHTML = 
-    `<section id="_about" class="about-content">
-            <div class="row">
-                <h1>About Us</h1>
-                <p>
-                A restaurant is a place where cooked food is sold to the public, and where people sit down to eat it. It is also a place where people go to enjoy the time and to eat a meal. Some restaurants are a chain, meaning that there are restaurants which have the same name and serve the same food.
-                </p>
-                <p>
-                There are also chain restaurants that serve slightly more expensive food. They are called fast casual restaurants. Applebee's and Perkins are examples of this type of chain restaurant.
-                </p>
-            </div>
-        </section>
+let content = document.querySelector('main');
+content.innerHTML = 
+`<section id="_about" class="about-content">
+<div class="row">
+    <h1>About Us</h1>
+    <p>
+    A restaurant is a place where cooked food is sold to the public, and where people sit down to eat it. It is also a place where people go to enjoy the time and to eat a meal. Some restaurants are a chain, meaning that there are restaurants which have the same name and serve the same food.
+    </p>
+    <p>
+    There are also chain restaurants that serve slightly more expensive food. They are called fast casual restaurants. Applebee's and Perkins are examples of this type of chain restaurant.
+    </p>
+</div>
+</section>
     `;
-}
+};
 
-export {about};
+export { about };
