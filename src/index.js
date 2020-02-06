d00e55d15caefaea39b0a0387fb21b2bda7d3544
dev1980 <brham1980@gmail.com>
2020-02-06 21:43:40 +0545

    fixed linter error

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './style.css';
-import{ homePage, homePageContent }from './header'; 
+import {homePage, homePageContent} from './header'; 
 import about from './about';
 import menu from './menu';
 import contact from './contact';
@@ -11,13 +11,11 @@ const tabs = document.querySelectorAll('.tab');
 
 tabs.forEach(tab => {
   tab.addEventListener('click', e => {
-      let tabName = tab.className.replace('tab ','');
-      let main = document.querySelector('main');
+    const tabName = tab.className.replace('tab ', '');
+    const main = document.querySelector('main');
 
-if (tabName === main.id) {
-    return;
-} 
-
+    if (tabName === main.id) {
+      return;} 
 switch(tabName) {
     case 'home': {
         main.id = "home";
