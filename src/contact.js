974eb24e0842efb94465e29406c4a198ab454d0b
dev1980 <brham1980@gmail.com>
2020-02-06 21:24:10 +0545

    linter error fixed

--- src/contact.js
+++ src/contact.js
@@ -1,13 +1,11 @@
 const contact = () => {
   const content = document.querySelector('main');
-  content.innerHTML = 
-    `<div class="row">
+  content.innerHTML = `<div class="row">
     <h1>Contact Us</h1>
     <p>Home Delivery : Call-804-9870-768</p>
     <p>Table Booking: Call-804-9870-768</p>
     <p>Email: jone@gmail.com</p> 
-    </div>
-    `;
+    </div>`;
 };
 
 export default contact;
--- src/index.js
+++ src/index.js
@@ -4,43 +4,42 @@ import about from './about';
 import menu from './menu';
 import contact from './contact';
 
-
 homePage();
 homePageContent();
 
-let tabs = document.querySelectorAll('.tab');
+const tabs = document.querySelectorAll('.tab');
 
 tabs.forEach(tab => {
-    tab.addEventListener('click', e => {
-        let tabName = tab.className.replace('tab ','');
-        let main = document.querySelector('main');
+  tab.addEventListener('click', e => {
+      let tabName = tab.className.replace('tab ','');
+      let main = document.querySelector('main');
 
-        if(tabName == main.id) {
-            return;
-        } 
+    if (tabName === main.id) {
+        return;
+    } 
 
-        switch(tabName) {
-            case 'home': {
-                main.id = "home";
-                homePageContent();                
-                break;
-            }
-            case 'about': {
-                main.id = "about";
-                about();
-                break;
-            }
-            case 'menu': {
-                main.id = "menu";
-                menu();
-                break;
-            }
-            case 'contact': {
-                main.id = "contact";
-                contact();
-                break;
-            }
+    switch(tabName) {
+        case 'home': {
+            main.id = "home";
+            homePageContent();                
+            break;
         }
+        case 'about': {
+            main.id = "about";
+            about();
+            break;
+        }
+        case 'menu': {
+            main.id = "menu";
+            menu();
+            break;
+        }
+        case 'contact': {
+            main.id = "contact";
+            contact();
+            break;
+        }
+    }
 
     });
 });
